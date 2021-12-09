4d0ba50e0d9ab162cbc52256fa81cbf6b12fec6a
don4t4s <donatas2x2@gmail.com>
2021-12-09 16:23:41 +0200

    User registration....

--- lib/pages/registration/registration_page.dart
+++ lib/pages/registration/registration_page.dart
@@ -1,34 +1,28 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rainbow_challenge/utils/repository/user_repository.dart';
-
-import 'package:rainbow_challenge/bloc/authentication_bloc.dart';
 import 'package:rainbow_challenge/pages/registration/bloc/reg_bloc.dart';
 import 'package:rainbow_challenge/pages/registration/registration_form.dart';
+import 'package:rainbow_challenge/utils/repository/user_repository.dart';
 
 class RegistrationPage extends StatelessWidget {
-  final UserRepository userRepository;
 
-  RegistrationPage({Key? key, required this.userRepository})
-      : assert(userRepository != null),
-        super(key: key);
+  final UserRepository userRepository;
+  const RegistrationPage({Key? key, required this.userRepository}) : assert(userRepository != null), super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Registracija: | Naujas vartotojas'),
-      ),
-      body: BlocProvider(
-        create: (context) {
-          return RegistrationBloc(
-            authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
-            userRepository: userRepository,
-          );
-        },
-        child: RegistrationForm(),
-      ),
+        appBar: AppBar(
+          title: Text('Naujas vartotojas'),
+          elevation: 0,
+        ),
+        body: SafeArea(
+          top: false,
+          child: BlocProvider(
+            create: (_) => RegistrationBloc(userRepository: userRepository),
+            child: RegistrationForm(),
+          ),
+        )
     );
   }
 }
