3f29407f33600355a8505883fa289488a31cae4e
Ignas Bagdonas <ignas.bagdonas@citybeesolutions.lt>
2021-12-17 10:33:28 +0200

    Fixed login navigation

--- lib/pages/login/bloc/login_state.dart
+++ lib/pages/login/bloc/login_state.dart
@@ -11,6 +11,8 @@ class LoginInitial extends LoginState {}
 
 class LoginLoading extends LoginState {}
 
+class LoginSuccess extends LoginState {}
+
 class LoginFailure extends LoginState {
   final String error;
 
