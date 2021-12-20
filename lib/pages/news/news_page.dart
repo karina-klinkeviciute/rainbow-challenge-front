1fe3a3abe601f1939190079048f73ebfc3ca2645
Ieva Sargautyte <ieva@skyhook.se>
2021-12-20 14:20:11 +0100

    WIP simplify files structure for news page

--- lib/pages/news/news_page.dart
+++ lib/pages/news/news_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rainbow_challenge/services/dio_client.dart';
 import 'package:rainbow_challenge/theme/colors.dart';
 import 'package:rainbow_challenge/theme/fonts.dart' as ThemeFonts;
+import 'package:rainbow_challenge/utils/model/models.dart';
 import 'package:rainbow_challenge/widgets/html.dart';
 import '../pages.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -12,8 +13,6 @@ import 'package:rainbow_challenge/utils/repository/repositories.dart';
 import '../challenges/cubit/challenges_cubit.dart';
 
 import 'cubit/news_cubit.dart';
-part 'part_news_list.dart';
-part 'part_tabs.dart';
 
 class NewsPage extends StatelessWidget {
   NewsPage({Key? key}) : super(key: key);
@@ -35,7 +34,7 @@ class NewsPage extends StatelessWidget {
                     NewsCubit(newsRepository: newsRepository),
                 child: SafeArea(
                   top: true,
-                  child: _newsList(context),
+                  child: _NewsList(),
                 ),
               ),
               ProfilePage(),
@@ -48,7 +47,87 @@ class NewsPage extends StatelessWidget {
               NavigationPage(),
             ],
           ),
-          bottomNavigationBar: _tabs(context)),
+          bottomNavigationBar: _BottomTabs()),
     );
+  }
+}
+
+class _NewsList extends StatelessWidget {
+  const _NewsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    BlocProvider.of<NewsCubit>(context).fetchNews();
+    return BlocBuilder<NewsCubit, NewsState>(builder: (context, state) {
+      if (!(state is NewsLoaded))
+        return Center(child: CircularProgressIndicator());
+      final newsList = (state).newsList;
+
+      return ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: newsList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return newsList.map((e) => _newsItem(e, context)).toList()[index];
+          });
+    });
+  }
+
+  Widget _newsItem(News newsItem, context) {
+    return Row(
+      children: [
+        Text(newsItem.title),
+        if (newsItem.image != null) ImageExternalWidget(url: newsItem.image!),
+        HtmlWidget(data: newsItem.body)
+      ],
+    );
+  }
+}
+
+class _BottomTabs extends StatelessWidget {
+  const _BottomTabs({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: SizedBox(
+            height: 55,
+            child: TabBar(
+              tabs: [
+                Tab(
+                  text: AppLocalizations.of(context)!.menu_home,
+                  icon: Icon(Icons.home),
+                  iconMargin: EdgeInsets.zero,
+                ),
+                Tab(
+                  text: AppLocalizations.of(context)!.menu_profile,
+                  icon: Icon(Icons.account_circle),
+                  iconMargin: EdgeInsets.zero,
+                ),
+                Tab(
+                  text: AppLocalizations.of(context)!.menu_challenges,
+                  icon: Icon(Icons.star_outline),
+                  iconMargin: EdgeInsets.zero,
+                ),
+                Tab(
+                  text: AppLocalizations.of(context)!.menu_regions,
+                  icon: Icon(Icons.place),
+                  iconMargin: EdgeInsets.zero,
+                ),
+                Tab(
+                  text: AppLocalizations.of(context)!.menu_navigation,
+                  icon: Icon(Icons.menu),
+                  iconMargin: EdgeInsets.zero,
+                ),
+              ],
+              indicatorColor: ThemeColors.secondaryColor,
+              labelColor: ThemeColors.secondaryColor,
+              unselectedLabelColor: ThemeColors.neutralColorLight,
+              unselectedLabelStyle: const TextStyle(
+                  fontFamily: ThemeFonts.primaryFontFamily, fontSize: 14),
+              labelStyle: const TextStyle(
+                  fontFamily: ThemeFonts.primaryFontFamily, fontSize: 14),
+            )));
   }
 }
