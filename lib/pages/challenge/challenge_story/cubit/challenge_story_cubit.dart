0c03b770f4cd603bf3b5021807c1e21843859584
Ignas Bagdonas <ignas.bagdonas@citybeesolutions.lt>
2022-03-12 12:04:51 +0200

    WIP

--- lib/pages/challenge/challenge_story/cubit/challenge_story_cubit.dart
+++ lib/pages/challenge/challenge_story/cubit/challenge_story_cubit.dart
@@ -12,13 +12,14 @@ class ChallengeStoryCubit extends Cubit<ChallengeStoryState> {
   ChallengeStoryCubit({required this.challengesTypeRepository})
       : super(ChallengeStoryInitial());
 
-  void fetchChallenge({required String uuid}) {
-    Timer(Duration(seconds: 1), () {
-      challengesTypeRepository
-          .fetchChallenge(uuid: uuid)
-          .then((challengeTypeItem) {
-        emit(ChallengeStoryLoaded(challengeTypeItem: challengeTypeItem));
-      });
-    });
+  Future<ChallengeStory> fetchChallenge(
+      {required String uuid, required String type_uuid}) async {
+    var challenge =
+        await challengesTypeRepository.fetchChallenge(uuid: type_uuid);
+    var joinedChallenge =
+        await challengesTypeRepository.fetchJoinedChallenge(uuid: uuid);
+
+    emit(ChallengeStoryLoaded(challengeTypeItem: challenge));
+    return joinedChallenge;
   }
 }
--- lib/utils/model/challenge/challenge_story/challenge_story_class.g.dart
+++ lib/utils/model/challenge/challenge_story/challenge_story_class.g.dart
@@ -8,13 +8,17 @@ part of 'challenge_story_class.dart';
 
 ChallengeStory _$ChallengeStoryFromJson(Map<String, dynamic> json) =>
     ChallengeStory(
-      uuid: json['uuid'] as String,
-      main_challenge:
-          Challenge.fromJson(json['main_challenge'] as Map<String, dynamic>),
-    );
+        uuid: json['uuid'] as String,
+        main_challenge: json['main_challenge'] != null
+            ? Challenge.fromJson(json['main_challenge'] as Map<String, dynamic>)
+            : Challenge.empty(),
+        description: json['description'],
+        story_url: json['story_url']);
 
 Map<String, dynamic> _$ChallengeStoryToJson(ChallengeStory instance) =>
     <String, dynamic>{
       'uuid': instance.uuid,
       'main_challenge': instance.main_challenge.toJson(),
+      'story_url': instance.story_url,
+      'description': instance.description
     };
