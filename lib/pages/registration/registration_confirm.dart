8c590cc11e3baf46c4439f3ac4e29f5ec4dbaa5e
unknown <romayorg@gmail.com>
2022-06-13 15:59:57 +0300

    added some text and button

--- lib/pages/registration/registration_confirm.dart
+++ lib/pages/registration/registration_confirm.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:rainbow_challenge/pages/login/login_page.dart';
+import 'package:rainbow_challenge/utils/repository/user_repository.dart';
 
 class RegistrationPageConfirm extends StatelessWidget {
   const RegistrationPageConfirm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      //  backgroundColor: ThemeColors.bgColorLight,
-        body: SafeArea(
-            bottom: true,
-            top: true,
-            child: Text('Registracija sėkminga.')
-        )
-    );
+    return Scaffold(
+        //  backgroundColor: ThemeColors.bgColorLight,
+        body: Padding(
+      padding: EdgeInsets.all(30.0),
+      child: SafeArea(
+        bottom: true,
+        top: true,
+        child: Column(
+          children: [
+            Text(
+              'Registracija sėkminga. Prisijungti galėsi po to, kai aktyvuosi paskyrą. Aktyvavimo nuorodą nusiuntėme nurodytu el. pašto adresu.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              child: Text('Prisijungti'),
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(
+                        userRepository: UserRepository(),
+                      ),
+                    ));
+              },
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
