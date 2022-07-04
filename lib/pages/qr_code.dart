8c8e14bc75d58db0e88bed20ecb8116f6ff35868
unknown <romayorg@gmail.com>
2022-07-04 18:28:39 +0300

    remain to check correct reply from api

--- lib/pages/qr_code.dart
+++ lib/pages/qr_code.dart
@@ -1,28 +1,176 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:rainbow_challenge/theme/colors.dart';
-import 'package:rainbow_challenge/theme/icons.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rainbow_challenge/pages/challenge/challenge_event_participant/qr_code_scanner/cubit/qr_code_scanner_cubit.dart';
+import 'package:rainbow_challenge/widgets/auth_text_field.dart';
 import 'package:rainbow_challenge/widgets/widgets.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class QrCodePage extends StatelessWidget {
-  const QrCodePage({Key? key}) : super(key: key);
+class QrCodePage extends StatefulWidget {
+  QrCodePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  QrCodePageState createState() {
+    return QrCodePageState();
+  }
+}
+
+class QrCodePageState extends State<QrCodePage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR code');
+  Barcode? result;
+  String? qrCode;
+  QRViewController? controller;
+
+  QrCodePageState({this.qrCode});
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 10,
+              child: QRView(
+                key: qrKey,
+                onQRViewCreated: _onQRViewCreated,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: Text('Paliesk viršuje, kad skenuotum QR kodą'),
+              ),
+            ),
+            //in case we need to add manually qr code
+            // Expanded(
+            //   flex: 1,
+            //   child: AuthTextField(
+            //       padding: EdgeInsets.symmetric(
+            //         horizontal: MediaQuery.of(context).size.width * 0.35,
+            //       ),
+            //       hint: 'Lytis, jei kita',
+            //       key: const Key('GenderOther_textField'),
+            //       isRequiredField: false,
+            //       keyboardType: TextInputType.text,
+            //       onChanged: (value) => setState(() async {
+            //             qrCode = value;
+            //             await completeChallenge();
+            //           })),
+            // )
+          ],
+        ));
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) async {
+      controller.pauseCamera();
+      setState(() {
+        result = scanData;
+      });
+      print("QR scanned");
+      await scanChallenge();
+      controller.resumeCamera();
+    });
+  }
+
+  Future<void> scanChallenge() async {
+    var qrCodeScannerCubit = BlocProvider.of<QrCodeScannerCubit>(context);
+    String qrCode = result?.code ?? "";
+    try {
+      var response = await qrCodeScannerCubit.challengeRepository
+          .scanChallenge(qr_code: qrCode);
+      bool success = false;
+
+      success = true;
+      setState(() {});
+      await showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          elevation: 80,
+          title: const Text('Ačiū'),
+          content: Text('$response'), //TODO add localizations
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, 'OK');
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+      Navigator.pop(context, success);
+    } catch (e) {
+      var er = e.toString();
+      if (er.contains(RegExp('[)]|[(]|[}]|[{]|\\]|[[]'))) {
+        er = er.replaceAll(RegExp('[)]|[(]|[}]|[{]|\\]|[[]'), '');
+        setState(() {});
+      }
+      await showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          elevation: 80,
+          title: const Text('Ačiū'),
+          content: Text('${er}'), //TODO add localizations
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, 'OK');
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
+
+class _Page extends StatelessWidget {
+  _Page({Key? key, required this.uuid}) : super(key: key);
+  final String uuid;
 
   @override
   Widget build(BuildContext context) {
     return WrapperMainWidget(
-      mainArea: _MainArea(),
-      bodyBackgroundColor: ThemeColors.bgColorLight,
-    );
+        mainArea: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Container()));
   }
 }
 
-class _MainArea extends StatelessWidget {
-  const _MainArea({Key? key}) : super(key: key);
+// Next: How do I get a joined challenge type uuid? It's needed to post together
+// With the qr code.
 
-  @override
-  Widget build(BuildContext context) {
-    return MessageWidget(
-      icon: ThemeIcons.qrCode,
-      title: 'Čia galėsi skenuoti QR Kodą',
-    );
-  }
-}
+// Process: 
+// Challenges page: concrete_challenge_uuid = challenge type uuid; 
+// If I know challenge type uuid, how do I get joined challenge type uuid then?
+// Do i need it at all for completing the challenge?
+
+// BlocBuilder 
+// Form validation 
+// completeChallenge() to API 
