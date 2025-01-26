0e00ce6518487361f380179cfc784af9e0d48ef7
Karina Klinkevičiūtė <karina.klinkeviciute@gmail.com>
2025-01-26 18:09:49 +0200

    Portuguese and Slovakian regions

--- lib/constants/drop_down_lists.dart
+++ lib/constants/drop_down_lists.dart
@@ -3,48 +3,153 @@
 class RegionDropDownLists {
   static const regionList = [
     {
-      "name": "Kauno rajonas",
-      "uuid": "427e33d7-c952-4501-aee0-193ac0603c1e",
-      "points": 0
-    },
-    {
-      "name": "Klaipėdos miestas",
-      "uuid": "df524a40-8019-40a0-8759-2178b9bcaf73",
-      "points": 0
+      "name": "Vilnius",
+      "uuid": "e3f37d13-abee-4159-a5f4-a85162fccba8",
+      "points": 5016
     },
     {
       "name": "Kauno miestas",
       "uuid": "b6a9d54f-36f9-4528-954d-afcb39dde22d",
-      "points": 15
-    },
-    {
-      "name": "Kėdainių miestas",
-      "uuid": "a996af32-2493-44d6-82bb-2ad12d1924ab",
-      "points": 0
-    },
-    {
-      "name": "Šiaulių miestas",
-      "uuid": "a8bd9bbb-af17-483e-8b10-d04fa86a8f26",
-      "points": 0
-    },
-    {
-      "name": "Palangos miestas",
-      "uuid": "877ad8e5-0a16-491f-9922-247a7eada0c5",
-      "points": 0
+      "points": 901
     },
     {
       "name": "Panevėžio miestas",
       "uuid": "10eed856-ad1c-44c6-8a79-decba92304c5",
-      "points": 0
+      "points": 641
     },
     {
-      "name": "Akmenės rajonas",
-      "uuid": "2e7033d7-fc16-424e-859d-397c67af171b",
-      "points": 0
+      "name": "Užsienis",
+      "uuid": "7fd88ac0-3245-4850-9145-0b6e45221b0d",
+      "points": 563
+    },
+    {
+      "name": "Klaipėdos miestas",
+      "uuid": "df524a40-8019-40a0-8759-2178b9bcaf73",
+      "points": 418
+    },
+    {
+      "name": "Elektrėnai",
+      "uuid": "355b404a-fe4a-4915-9577-abb60cc173dd",
+      "points": 417
+    },
+    {
+      "name": "Šiaulių miestas",
+      "uuid": "a8bd9bbb-af17-483e-8b10-d04fa86a8f26",
+      "points": 368
+    },
+    {
+      "name": "Šilutės rajonas",
+      "uuid": "75cf5b64-a9c7-47fe-b822-6aac676ebfbe",
+      "points": 349
+    },
+    {
+      "name": "Marijampolė",
+      "uuid": "ca543f11-9043-4336-a4e1-eba55e5bd065",
+      "points": 348
+    },
+    {
+      "name": "Ignalinos rajonas",
+      "uuid": "725a506b-b30e-4d5a-8601-55cbc334a061",
+      "points": 295
+    },
+    {
+      "name": "Telšių rajonas",
+      "uuid": "23c3075d-9b40-4d82-996a-8884e2380f6c",
+      "points": 293
+    },
+    {
+      "name": "Druskininkai",
+      "uuid": "4431d23c-220b-4cdb-9ad8-0d436fc40b88",
+      "points": 205
+    },
+    {
+      "name": "Joniškio rajonas",
+      "uuid": "eb074f6e-189a-465c-8d83-fd0304f67d1b",
+      "points": 205
+    },
+    {
+      "name": "Jonavos rajonas",
+      "uuid": "4bbf60f7-9d37-4fb6-ad19-d0ba85b7a836",
+      "points": 184
+    },
+    {
+      "name": "Ukmergės rajonas",
+      "uuid": "741edd1c-f198-4f4c-9890-cb0a89a9fd89",
+      "points": 169
     },
     {
       "name": "Alytaus miestas",
       "uuid": "8e4c6124-fbb7-430c-a917-3f3e67629edf",
+      "points": 155
+    },
+    {
+      "name": "Tauragės rajonas",
+      "uuid": "5001a6dc-14bf-48b6-a5ab-e0ae47d72e38",
+      "points": 148
+    },
+    {
+      "name": "Klaipėdos rajonas",
+      "uuid": "d48b76a7-ad3b-412d-92f6-391ffcab5a43",
+      "points": 146
+    },
+    {
+      "name": "Kauno rajonas",
+      "uuid": "427e33d7-c952-4501-aee0-193ac0603c1e",
+      "points": 118
+    },
+    {
+      "name": "Šalčininkų rajonas",
+      "uuid": "f6a3b500-e732-4a65-91f4-65020bb4123e",
+      "points": 104
+    },
+    {
+      "name": "Šiaulių rajonas",
+      "uuid": "3e5e5bf7-0615-464e-82b5-fe314bc9f233",
+      "points": 97
+    },
+    {
+      "name": "Utenos rajonas",
+      "uuid": "83552f19-fd4d-4247-a3ed-3bc7a620ec2b",
+      "points": 88
+    },
+    {
+      "name": "Kėdainių miestas",
+      "uuid": "a996af32-2493-44d6-82bb-2ad12d1924ab",
+      "points": 60
+    },
+    {
+      "name": "Vilniaus rajonas",
+      "uuid": "612e6940-09c4-47cc-8f42-8f7c8c480c38",
+      "points": 55
+    },
+    {
+      "name": "Palangos miestas",
+      "uuid": "877ad8e5-0a16-491f-9922-247a7eada0c5",
+      "points": 49
+    },
+    {
+      "name": "Kėdainių rajonas",
+      "uuid": "2895d02c-2157-4deb-88c8-b87418ce4d07",
+      "points": 30
+    },
+    {
+      "name": "Birštonas",
+      "uuid": "1e2ef7fe-3bd9-4ce4-ac80-8f23dc454a4d",
+      "points": 23
+    },
+    {
+      "name": "Pasvalio rajonas",
+      "uuid": "f949b0b2-3f1a-4634-af30-f187e61eb453",
+      "points": 20
+    },
+    {
+      "name": "Šilalės rajonas",
+      "uuid": "e1ccad8d-4ad4-4cd8-819d-20cc5d71ffa4",
+      "points": 4
+    },
+    {
+      "name": "Akmenės rajonas",
+      "uuid": "2e7033d7-fc16-424e-859d-397c67af171b",
       "points": 0
     },
     {
@@ -58,38 +163,8 @@ class RegionDropDownLists {
       "points": 0
     },
     {
-      "name": "Birštonas",
-      "uuid": "1e2ef7fe-3bd9-4ce4-ac80-8f23dc454a4d",
-      "points": 0
-    },
-    {
       "name": "Biržai",
       "uuid": "756ee3e8-8abe-4b95-88e8-7d7745a4cd58",
-      "points": 0
-    },
-    {
-      "name": "Druskininkai",
-      "uuid": "4431d23c-220b-4cdb-9ad8-0d436fc40b88",
-      "points": 0
-    },
-    {
-      "name": "Elektrėnai",
-      "uuid": "355b404a-fe4a-4915-9577-abb60cc173dd",
-      "points": 0
-    },
-    {
-      "name": "Ignalinos rajonas",
-      "uuid": "725a506b-b30e-4d5a-8601-55cbc334a061",
-      "points": 0
-    },
-    {
-      "name": "Jonavos rajonas",
-      "uuid": "4bbf60f7-9d37-4fb6-ad19-d0ba85b7a836",
-      "points": 0
-    },
-    {
-      "name": "Joniškio rajonas",
-      "uuid": "eb074f6e-189a-465c-8d83-fd0304f67d1b",
       "points": 0
     },
     {
@@ -118,16 +193,6 @@ class RegionDropDownLists {
       "points": 0
     },
     {
-      "name": "Kėdainių rajonas",
-      "uuid": "2895d02c-2157-4deb-88c8-b87418ce4d07",
-      "points": 0
-    },
-    {
-      "name": "Klaipėdos rajonas",
-      "uuid": "d48b76a7-ad3b-412d-92f6-391ffcab5a43",
-      "points": 0
-    },
-    {
       "name": "Kretingos rajonas",
       "uuid": "721bbcd2-c989-4549-b92e-658e92fbb61f",
       "points": 0
@@ -140,11 +205,6 @@ class RegionDropDownLists {
     {
       "name": "Lazdijų rajonas",
       "uuid": "2c33d576-987a-4b06-a4c3-7a1420854add",
-      "points": 0
-    },
-    {
-      "name": "Marijampolė",
-      "uuid": "ca543f11-9043-4336-a4e1-eba55e5bd065",
       "points": 0
     },
     {
@@ -175,11 +235,6 @@ class RegionDropDownLists {
     {
       "name": "Panevėžio rajonas",
       "uuid": "5de7751a-c023-4ce3-9258-f95920933143",
-      "points": 0
-    },
-    {
-      "name": "Pasvalio rajonas",
-      "uuid": "f949b0b2-3f1a-4634-af30-f187e61eb453",
       "points": 0
     },
     {
@@ -218,26 +273,6 @@ class RegionDropDownLists {
       "points": 0
     },
     {
-      "name": "Šalčininkų rajonas",
-      "uuid": "f6a3b500-e732-4a65-91f4-65020bb4123e",
-      "points": 0
-    },
-    {
-      "name": "Šiaulių rajonas",
-      "uuid": "3e5e5bf7-0615-464e-82b5-fe314bc9f233",
-      "points": 0
-    },
-    {
-      "name": "Šilalės rajonas",
-      "uuid": "e1ccad8d-4ad4-4cd8-819d-20cc5d71ffa4",
-      "points": 0
-    },
-    {
-      "name": "Šilutės rajonas",
-      "uuid": "75cf5b64-a9c7-47fe-b822-6aac676ebfbe",
-      "points": 0
-    },
-    {
       "name": "Širvintų rajonas",
       "uuid": "e5154cd8-7092-47db-8849-585376b75558",
       "points": 0
@@ -248,28 +283,8 @@ class RegionDropDownLists {
       "points": 0
     },
     {
-      "name": "Tauragės rajonas",
-      "uuid": "5001a6dc-14bf-48b6-a5ab-e0ae47d72e38",
-      "points": 0
-    },
-    {
-      "name": "Telšių rajonas",
-      "uuid": "23c3075d-9b40-4d82-996a-8884e2380f6c",
-      "points": 0
-    },
-    {
       "name": "Trakų rajonas",
       "uuid": "8f249beb-bfdd-41e6-9b65-d646054f9ec7",
-      "points": 0
-    },
-    {
-      "name": "Ukmergės rajonas",
-      "uuid": "741edd1c-f198-4f4c-9890-cb0a89a9fd89",
-      "points": 0
-    },
-    {
-      "name": "Utenos rajonas",
-      "uuid": "83552f19-fd4d-4247-a3ed-3bc7a620ec2b",
       "points": 0
     },
     {
@@ -280,16 +295,6 @@ class RegionDropDownLists {
     {
       "name": "Vilkaviškio rajonas",
       "uuid": "5e9b2b5d-ff7b-4bfd-bdd9-2cdf60ffcaa8",
-      "points": 0
-    },
-    {
-      "name": "Vilnius",
-      "uuid": "e3f37d13-abee-4159-a5f4-a85162fccba8",
-      "points": 0
-    },
-    {
-      "name": "Vilniaus rajonas",
-      "uuid": "612e6940-09c4-47cc-8f42-8f7c8c480c38",
       "points": 0
     },
     {
@@ -308,8 +313,133 @@ class RegionDropDownLists {
       "points": 0
     },
     {
-      "name": "Užsienis",
-      "uuid": "7fd88ac0-3245-4850-9145-0b6e45221b0d",
+      "name": "Lisbon",
+      "uuid": "9c46ae09-1173-4964-bbba-52f9f415c915",
+      "points": 0
+    },
+    {
+      "name": "Leiria",
+      "uuid": "1886950b-0626-429f-ac00-66f7c5dd4fad",
+      "points": 0
+    },
+    {
+      "name": "Santarém",
+      "uuid": "09ea30f0-c38a-435d-a7c8-438c5cbe4361",
+      "points": 0
+    },
+    {
+      "name": "Setúbal",
+      "uuid": "4e2bbf07-b5f3-4d7f-9037-bcdfb5a96811",
+      "points": 0
+    },
+    {
+      "name": "Beja",
+      "uuid": "a97d28ca-9780-482e-969c-732d43830415",
+      "points": 0
+    },
+    {
+      "name": "Faro",
+      "uuid": "46c0822e-f8e9-4ae2-bc56-2946e5877121",
+      "points": 0
+    },
+    {
+      "name": "Évora",
+      "uuid": "9c6b77c8-d6f0-442c-9fd4-dbce810cdd34",
+      "points": 0
+    },
+    {
+      "name": "Portalegre",
+      "uuid": "048c4b78-c682-4209-802c-024138aadaa3",
+      "points": 0
+    },
+    {
+      "name": "Castelo Branco",
+      "uuid": "421c8fb3-f30d-46ed-b3a8-fe42bf5ee6fe",
+      "points": 0
+    },
+    {
+      "name": "Guarda",
+      "uuid": "5bdf58dd-dae8-45ba-a00b-743ea299d5a9",
+      "points": 0
+    },
+    {
+      "name": "Coimbra",
+      "uuid": "bc7c8b05-2aea-43ac-97c6-642684a2da19",
+      "points": 0
+    },
+    {
+      "name": "Aveiro",
+      "uuid": "bc912993-bb49-4c62-90ad-ca7871a2d087",
+      "points": 0
+    },
+    {
+      "name": "Viseu",
+      "uuid": "4c460fc2-6d91-44e6-aa05-dd02c0b66d3f",
+      "points": 0
+    },
+    {
+      "name": "Bragança",
+      "uuid": "b778fcc9-600c-43cd-879a-779923648507",
+      "points": 0
+    },
+    {
+      "name": "Vila Real",
+      "uuid": "5473d42a-45bb-4396-b2fe-9744e2d4a002",
+      "points": 0
+    },
+    {
+      "name": "Porto",
+      "uuid": "0342293c-992a-4eeb-bb17-a6850117931f",
+      "points": 0
+    },
+    {
+      "name": "Braga",
+      "uuid": "716bfb3f-93f6-4d1d-9985-3ae2a7f7c9ba",
+      "points": 0
+    },
+    {
+      "name": "Viana do Castelo",
+      "uuid": "f385e9b4-9795-47fe-b912-16b5ac263ea6",
+      "points": 0
+    },
+    {
+      "name": "Bratislava",
+      "uuid": "41e157c9-ef66-40e1-8088-3902145a1216",
+      "points": 0
+    },
+    {
+      "name": "Trnava",
+      "uuid": "9307e42e-47c5-4d4c-ba71-6b5ed5954ef2",
+      "points": 0
+    },
+    {
+      "name": "Trenčín",
+      "uuid": "671c3f43-eae3-4797-8ed4-eeda3beba7fb",
+      "points": 0
+    },
+    {
+      "name": "Nitra",
+      "uuid": "3ded1bc0-4421-43de-9b8c-a62df93c77fe",
+      "points": 0
+    },
+    {
+      "name": "Žilina",
+      "uuid": "5e745cc8-fadc-4013-9f60-a9781df34fb3",
+      "points": 0
+    },
+    {
+      "name": "Banská Bystrica",
+      "uuid": "395908dd-fa0f-4ef6-95c5-ce223cbb60a7",
+      "points": 0
+    },
+    {
+      "name": "Prešov",
+      "uuid": "d39aabde-b1e5-4eaa-8a66-28017b2afd23",
+      "points": 0
+    },
+    {
+      "name": "Košice",
+      "uuid": "f57f3cf6-2efd-4b02-85a6-9d5af13b4094",
       "points": 0
     }
   ];
