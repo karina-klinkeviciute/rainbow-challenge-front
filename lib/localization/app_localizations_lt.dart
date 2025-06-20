6dfedefa84174a28f7db991b8c8638f595cad25c
Karina Klinkevičiūtė <karina.klinkeviciute@gmail.com>
2025-06-20 21:41:44 +0300

    newest flutter version

---
+++ lib/localization/app_localizations_lt.dart
@@ -0,0 +1,417 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Lithuanian (`lt`).
+class AppLocalizationsLt extends AppLocalizations {
+  AppLocalizationsLt([String locale = 'lt']) : super(locale);
+
+  @override
+  String get app_title => 'Rainbow Challenge';
+
+  @override
+  String get email => 'El. paštas';
+
+  @override
+  String get enter_email => 'Įvesk el. pašto adresą';
+
+  @override
+  String get email_enter_valid => 'Prašom įvesti tinkamą el. pašto adresą';
+
+  @override
+  String get password => 'Slaptažodis';
+
+  @override
+  String get testing => 'Test';
+
+  @override
+  String get new_user => 'Naujas naudotojas';
+
+  @override
+  String get change_password => 'Keisti slaptažodį';
+
+  @override
+  String get password_hint => 'Slaptažodis';
+
+  @override
+  String get repeat_password => 'Pakartokite slaptažodį';
+
+  @override
+  String get current_password => 'Dabartinis slaptažodis';
+
+  @override
+  String get passwords_must_match => 'Slaptažodžiai turi sutapti';
+
+  @override
+  String get password_rules =>
+      'Slaptažodis turi būti ne trumpesnis negu 8 simboliai, iš jų bent 1 skaičius';
+
+  @override
+  String get password_update_success => 'Slaptažodis pakeistas sėkmingai';
+
+  @override
+  String get password_recovery_url =>
+      'Slaptažodžio atstatymo nuoroda nusiųsta nurodytu el. pašto adresu';
+
+  @override
+  String get registration_successful =>
+      'Registracija sėkminga. Prisijungti galėsi po to, kai aktyvuosi paskyrą. Aktyvavimo nuorodą nusiuntėme nurodytu el. pašto adresu.';
+
+  @override
+  String get user_settings => 'Naudotojo nustatymai';
+
+  @override
+  String get settings_changed => 'Nustatymai pakeisti';
+
+  @override
+  String get action_back_to_profile => 'Atgal į profilį';
+
+  @override
+  String get action_back_to_challenges => 'Atgal į užduotis';
+
+  @override
+  String get action_login => 'Prisijungti';
+
+  @override
+  String get action_logout => 'Atsijungti';
+
+  @override
+  String get action_submit => 'Pateikti';
+
+  @override
+  String get action_register => 'Neturi paskyros? Registruokis';
+
+  @override
+  String get action_have_account_login => 'Turi paskyrą? Prisijunk';
+
+  @override
+  String get action_forgot_password => 'Užmiršai slaptažodį? Spausk čia';
+
+  @override
+  String get action_update_settings => 'Atnaujinti nustatymus';
+
+  @override
+  String get action_back => 'Grįžti';
+
+  @override
+  String get menu_home => 'Pradžia';
+
+  @override
+  String get menu_profile => 'Profilis';
+
+  @override
+  String get menu_challenges => 'Užduotys';
+
+  @override
+  String get menu_regions => 'Regionai';
+
+  @override
+  String get menu_navigation => 'Navigacija';
+
+  @override
+  String get menu_challenges_joined => 'Pradėtos užduotys';
+
+  @override
+  String get menu_about => 'Apie';
+
+  @override
+  String get menu_qr_code => 'QR kodas';
+
+  @override
+  String get menu_shop => 'Parduotuvė';
+
+  @override
+  String get menu_messages => 'Žinutės';
+
+  @override
+  String get rules => 'Taisyklės';
+
+  @override
+  String get conditions => 'Sąlygos';
+
+  @override
+  String get home_empty_title =>
+      'Šiuo metu turinys ruošiamas, tačiau jau dabar gali pradėti spręsti užduotis!';
+
+  @override
+  String get gender => 'Lytis';
+
+  @override
+  String get gender_if_other => 'Lytis, jei kita';
+
+  @override
+  String get region => 'Regionas';
+
+  @override
+  String get gender_woman => 'Moteris';
+
+  @override
+  String get gender_man => 'Vyras';
+
+  @override
+  String get gender_non_binary => 'Nebinarinė';
+
+  @override
+  String get gender_other => 'Kita';
+
+  @override
+  String get gender_prefer_not_to_say => 'Nenoriu nurodyti';
+
+  @override
+  String get year_of_birth => 'Gimimo metai';
+
+  @override
+  String get challenge_type_article => 'Straipsniai ir medija';
+
+  @override
+  String get challenge_type_custom => 'Įvairūs';
+
+  @override
+  String get challenge_type_event => 'Dalyvavimas renginiuose';
+
+  @override
+  String get challenge_type_event_org => 'Renginio organizavimas';
+
+  @override
+  String get challenge_type_project => 'Projekto įgyvendinimas';
+
+  @override
+  String get challenge_type_quiz => 'Žinių patikrinimas';
+
+  @override
+  String get challenge_type_reacting => 'Reagavimas į įvykius';
+
+  @override
+  String get challenge_type_school_gsa => 'Mokyklų GSA';
+
+  @override
+  String get challenge_type_story => 'Pasidalink savo istorija';
+
+  @override
+  String get challenge_type_support => 'Parama';
+
+  @override
+  String get challenge_type_completed => 'Atliktos užduotys';
+
+  @override
+  String get challenge_event_name_hint_text => 'Renginio pavadinimas *';
+
+  @override
+  String get challenge_event_description_hint_text => 'Renginio aprašymas *';
+
+  @override
+  String get challenge_event_link_hint_text => 'Renginio nuoroda *';
+
+  @override
+  String get challenge_event_organizing_alone => 'Organizuoju viena/s';
+
+  @override
+  String get challenge_event_touch_black_screen_for_qr =>
+      'Paliesk juodą ekraną QR kodo skenavimui';
+
+  @override
+  String get challenge_event_qr_scanned => 'QR kodas nuskenuotas';
+
+  @override
+  String get challenge_event_qr_code_incorrect => 'QR kodas neteisingas';
+
+  @override
+  String get challenge_project_name_hint_text => 'Projekto pavadinimas *';
+
+  @override
+  String get challenge_project_description_hint_text =>
+      'Description of the project *';
+
+  @override
+  String get challenge_project_link_hint_text => 'Projekto nuoroda *';
+
+  @override
+  String get challenge_project_completed_alone => 'Atlikau viena/s';
+
+  @override
+  String get action_join_challenge => 'Pradėk užduotį';
+
+  @override
+  String get joined_challenge_event_field_qr_code => 'Skenuoti QR kodą';
+
+  @override
+  String get joined_challenge_event_field_qr_code_wrong =>
+      'Atsiprašome, neteisingas QR kodas';
+
+  @override
+  String get joined_challenge_event_field_qr_code_right => 'Užduotis atlikta';
+
+  @override
+  String get correct_answers_amount => 'Teisingai atsakei įį tiek klausimų';
+
+  @override
+  String get rainbows_added_amount => 'Tau įskaičiuota tiek vaivorykščių';
+
+  @override
+  String get error => 'Klaida';
+
+  @override
+  String get error_unknown_error => 'Nenumatyta klaida, mėginkite dar kartą';
+
+  @override
+  String get message_fields_are_required => 'Laukai privalomi';
+
+  @override
+  String get message_field_is_required => 'Laukas privalomas';
+
+  @override
+  String get message_thank_you => 'Thank you!';
+
+  @override
+  String get message_challenge_completed_volunteer_will_take_a_look =>
+      'Ačiū už atliktą užduotį! Savanoris peržiūrės jūsų atsakymą ir įskaitys vaivorykštes. Apie tai būsite informuoti pranešimų skiltyje.';
+
+  @override
+  String get message_challenge_completed_rainbows_issued =>
+      'Ačiū už atliktą užduotį. Jums vaivorykštės įskaičiuotos.';
+
+  @override
+  String get message_changes_where_saved => 'Pakeitimai išsaugoti';
+
+  @override
+  String get challenge_completed_already => 'Šią užduotį jau atlikote';
+
+  @override
+  String get challenge_continue_single => 'Tęsti jau pradėtą užduotį';
+
+  @override
+  String get challenge_continue => 'Tęsti';
+
+  @override
+  String get challenge_save_and_submit_later => 'Saugoti ir pateikti vėliau';
+
+  @override
+  String get challenge_description_hint_text =>
+      'Aprašykite, kaip atlikote užduotį *';
+
+  @override
+  String get challenge_link_hint_text => 'Nuoroda *';
+
+  @override
+  String get challenges_no_completed_challenges => 'Atliktų užduočių nėra';
+
+  @override
+  String get challenges_no_joined_challenges => 'Pradėtų užduočių nėra';
+
+  @override
+  String get challenge_name => 'Užduoties pavadinimas';
+
+  @override
+  String get article_name_hint_text => 'Straipsnio pavadinimas *';
+
+  @override
+  String get article_url => 'Straipsnio nuoroda';
+
+  @override
+  String get profile_page_title => 'Mano profilis';
+
+  @override
+  String get profile_page_stats_rainbows => 'Vaivorykštės';
+
+  @override
+  String get profile_page_stats_active_weeks => 'Aktyvios savaitės';
+
+  @override
+  String get profile_page_medals => 'Medaliai';
+
+  @override
+  String get profile_page_achievements => 'Pasiekimai';
+
+  @override
+  String get action_challenge_end => 'Atlik šią užduotį iki';
+
+  @override
+  String get rainbows_summary => 'Vaivorykščių suvestinė';
+
+  @override
+  String get rainbows => 'Vaivorykštės';
+
+  @override
+  String get rainbows_received => 'Gautos Vaivorykštės';
+
+  @override
+  String get rainbows_active => 'Aktyvios Vaivorykštės';
+
+  @override
+  String get rainbows_received_for_tasks =>
+      'Vaivorykštes gavai už šias užduotis:';
+
+  @override
+  String get rainbows_spent_on => 'Vaivorykštes išleidai';
+
+  @override
+  String get shop_welcome_text =>
+      'Čia galėsi keisti vaivorykštes į mielus daikčiukus';
+
+  @override
+  String get shop_subtitle_text => 'Keisk vaivorykštes į mielus daikčiukus';
+
+  @override
+  String get price => 'Kaina';
+
+  @override
+  String get amount => 'Kiekis';
+
+  @override
+  String get total => 'Viso';
+
+  @override
+  String get order => 'Užsakyti';
+
+  @override
+  String get order_sent =>
+      'Užsakymas išsiųstas. Susisieksim su tavim el. paštu ir pranešim, kaip atsiimti prizus.';
+
+  @override
+  String get choose_amount => 'Pasirink kiekį';
+
+  @override
+  String get about_page_title => 'Apie projektą';
+
+  @override
+  String get about_page_logos => 'Projekto partneriai ir rėmėjai';
+
+  @override
+  String get messages_message => 'Pranešimas';
+
+  @override
+  String get delete_account => 'Ištrinti paskyrą';
+
+  @override
+  String get delete_account_confirm =>
+      'Paskyra bus ištrinta per 7 dienas. Apsigalvojai? Susisiek su info@rainbowchallenge.lt per 3 dienas.';
+
+  @override
+  String get wrong_password => 'Neteisingas slaptažodis!';
+
+  @override
+  String get user_logged_in => 'Prisijungęs';
+
+  @override
+  String get sign_in_with_google => 'Prisijungti su Google';
+
+  @override
+  String social_sign_in_failed(String provider) {
+    return 'Prisijungimas su $provider nepavyko';
+  }
+
+  @override
+  String get sign_in_with_apple => 'Prisijungti su Apple';
+
+  @override
+  String get files_attach_file => 'Prijungti failą';
+
+  @override
+  String get files_too_big =>
+      'Klaida. Failas, kurį bandote pridėti, per didelis';
+
+  @override
+  String get label_active => 'Aktyvios';
+}
---
+++ lib/localization/app_localizations_sk.dart
@@ -0,0 +1,415 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Slovak (`sk`).
+class AppLocalizationsSk extends AppLocalizations {
+  AppLocalizationsSk([String locale = 'sk']) : super(locale);
+
+  @override
+  String get app_title => 'Rainbow Challenge';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get enter_email => 'Zadajte e-mailovú adresu';
+
+  @override
+  String get email_enter_valid => 'Prosím, zadajte platnú e-mailovú adresu';
+
+  @override
+  String get password => 'Heslo';
+
+  @override
+  String get testing => 'Test';
+
+  @override
+  String get new_user => 'Nový používateľ';
+
+  @override
+  String get change_password => 'Zmeniť heslo';
+
+  @override
+  String get password_hint => 'Heslo';
+
+  @override
+  String get repeat_password => 'Zopakujte heslo';
+
+  @override
+  String get current_password => 'Aktuálne heslo';
+
+  @override
+  String get passwords_must_match => 'Heslá musia byť rovnaké';
+
+  @override
+  String get password_rules =>
+      'Heslo musí mať aspoň 8 znakov, z toho aspoň 1 číslo';
+
+  @override
+  String get password_update_success => 'Heslo bolo úspešne zmenené';
+
+  @override
+  String get password_recovery_url =>
+      'Odkaz na obnovenie hesla bol odoslaný na zadanú e-mailovú adresu';
+
+  @override
+  String get registration_successful =>
+      'Registrácia bola úspešná. Prihlásiť sa budete môcť po aktivácii účtu. Odkaz na aktiváciu sme poslali na zadanú e-mailovú adresu.';
+
+  @override
+  String get user_settings => 'Nastavenia používateľa';
+
+  @override
+  String get settings_changed => 'Nastavenia boli zmenené';
+
+  @override
+  String get action_back_to_profile => 'Späť na profil';
+
+  @override
+  String get action_back_to_challenges => 'Späť k úlohám';
+
+  @override
+  String get action_login => 'Prihlásiť sa';
+
+  @override
+  String get action_logout => 'Odhlásiť sa';
+
+  @override
+  String get action_submit => 'Odoslať';
+
+  @override
+  String get action_register => 'Nemáte účet? Zaregistrujte sa';
+
+  @override
+  String get action_have_account_login => 'Máte účet? Prihláste sa';
+
+  @override
+  String get action_forgot_password => 'Zabudli ste heslo? Kliknite sem';
+
+  @override
+  String get action_update_settings => 'Aktualizovať nastavenia';
+
+  @override
+  String get action_back => 'Vrátiť sa';
+
+  @override
+  String get menu_home => 'Domov';
+
+  @override
+  String get menu_profile => 'Profil';
+
+  @override
+  String get menu_challenges => 'Úlohy';
+
+  @override
+  String get menu_regions => 'Regióny';
+
+  @override
+  String get menu_navigation => 'Navigácia';
+
+  @override
+  String get menu_challenges_joined => 'Začaté úlohy';
+
+  @override
+  String get menu_about => 'O nás';
+
+  @override
+  String get menu_qr_code => 'QR kód';
+
+  @override
+  String get menu_shop => 'Obchod';
+
+  @override
+  String get menu_messages => 'Správy';
+
+  @override
+  String get rules => 'Pravidlá';
+
+  @override
+  String get conditions => 'Podmienky';
+
+  @override
+  String get home_empty_title =>
+      'Obsah sa momentálne pripravuje, ale už teraz môžete začať riešiť úlohy!';
+
+  @override
+  String get gender => 'Pohlavie';
+
+  @override
+  String get gender_if_other => 'Pohlavie, ak iné';
+
+  @override
+  String get region => 'Región';
+
+  @override
+  String get gender_woman => 'Žena';
+
+  @override
+  String get gender_man => 'Muž';
+
+  @override
+  String get gender_non_binary => 'Nebinárne';
+
+  @override
+  String get gender_other => 'Iné';
+
+  @override
+  String get gender_prefer_not_to_say => 'Nechcem uviesť';
+
+  @override
+  String get year_of_birth => 'Rok narodenia';
+
+  @override
+  String get challenge_type_article => 'Články a médiá';
+
+  @override
+  String get challenge_type_custom => 'Rôzne';
+
+  @override
+  String get challenge_type_event => 'Účasť na podujatiach';
+
+  @override
+  String get challenge_type_event_org => 'Organizovanie podujatia';
+
+  @override
+  String get challenge_type_project => 'Realizácia projektu';
+
+  @override
+  String get challenge_type_quiz => 'Test znalostí';
+
+  @override
+  String get challenge_type_reacting => 'Reakcia na udalosti';
+
+  @override
+  String get challenge_type_school_gsa => 'Školský GSA';
+
+  @override
+  String get challenge_type_story => 'Podeľte sa o svoj príbeh';
+
+  @override
+  String get challenge_type_support => 'Podpora';
+
+  @override
+  String get challenge_type_completed => 'Dokončené úlohy';
+
+  @override
+  String get challenge_event_name_hint_text => 'Názov podujatia *';
+
+  @override
+  String get challenge_event_description_hint_text => 'Popis podujatia *';
+
+  @override
+  String get challenge_event_link_hint_text => 'Odkaz na podujatie *';
+
+  @override
+  String get challenge_event_organizing_alone => 'Organizujem samostatne';
+
+  @override
+  String get challenge_event_touch_black_screen_for_qr =>
+      'Dotknite sa čiernej obrazovky pre skenovanie QR kódu';
+
+  @override
+  String get challenge_event_qr_scanned => 'QR kód bol naskenovaný';
+
+  @override
+  String get challenge_event_qr_code_incorrect => 'QR kód je nesprávny';
+
+  @override
+  String get challenge_project_name_hint_text => 'Názov projektu *';
+
+  @override
+  String get challenge_project_description_hint_text => 'Popis projektu *';
+
+  @override
+  String get challenge_project_link_hint_text => 'Odkaz na projekt *';
+
+  @override
+  String get challenge_project_completed_alone => 'Dokončil/a/o som samostatne';
+
+  @override
+  String get action_join_challenge => 'Začať úlohu';
+
+  @override
+  String get joined_challenge_event_field_qr_code => 'Naskenovať QR kód';
+
+  @override
+  String get joined_challenge_event_field_qr_code_wrong =>
+      'Prepáčte, nesprávny QR kód';
+
+  @override
+  String get joined_challenge_event_field_qr_code_right => 'Úloha dokončená';
+
+  @override
+  String get correct_answers_amount => 'Správne ste odpovedali na toľko otázok';
+
+  @override
+  String get rainbows_added_amount => 'Bolo vám pridaných toľko dúh';
+
+  @override
+  String get error => 'Chyba';
+
+  @override
+  String get error_unknown_error => 'Neočakávaná chyba, skúste to znova';
+
+  @override
+  String get message_fields_are_required => 'Polia sú povinné';
+
+  @override
+  String get message_field_is_required => 'Pole je povinné';
+
+  @override
+  String get message_thank_you => 'Ďakujeme!';
+
+  @override
+  String get message_challenge_completed_volunteer_will_take_a_look =>
+      'Ďakujeme za splnenie úlohy! Dobrovoľník skontroluje vašu odpoveď a pripočíta vám dúhy. Budete o tom informovaní v sekcii oznámení.';
+
+  @override
+  String get message_challenge_completed_rainbows_issued =>
+      'Ďakujeme za splnenie úlohy. Dúhy vám boli pripísané.';
+
+  @override
+  String get message_changes_where_saved => 'Zmeny boli uložené';
+
+  @override
+  String get challenge_completed_already => 'Túto úlohu ste už splnili';
+
+  @override
+  String get challenge_continue_single => 'Pokračovať v už začatej úlohe';
+
+  @override
+  String get challenge_continue => 'Pokračovať';
+
+  @override
+  String get challenge_save_and_submit_later => 'Uložiť a odoslať neskôr';
+
+  @override
+  String get challenge_description_hint_text =>
+      'Opíšte, ako ste splnili úlohu *';
+
+  @override
+  String get challenge_link_hint_text => 'Odkaz *';
+
+  @override
+  String get challenges_no_completed_challenges => 'Žiadne splnené úlohy';
+
+  @override
+  String get challenges_no_joined_challenges => 'Žiadne začaté úlohy';
+
+  @override
+  String get challenge_name => 'Názov úlohy';
+
+  @override
+  String get article_name_hint_text => 'Názov článku *';
+
+  @override
+  String get article_url => 'URL článku';
+
+  @override
+  String get profile_page_title => 'Môj profil';
+
+  @override
+  String get profile_page_stats_rainbows => 'Dúhy';
+
+  @override
+  String get profile_page_stats_active_weeks => 'Aktívne týždne';
+
+  @override
+  String get profile_page_medals => 'Medaily';
+
+  @override
+  String get profile_page_achievements => 'Úspechy';
+
+  @override
+  String get action_challenge_end => 'Splňte túto úlohu do';
+
+  @override
+  String get rainbows_summary => 'Súhrn dúh';
+
+  @override
+  String get rainbows => 'Dúhy';
+
+  @override
+  String get rainbows_received => 'Získané dúhy';
+
+  @override
+  String get rainbows_active => 'Aktívne dúhy';
+
+  @override
+  String get rainbows_received_for_tasks => 'Dúhy ste získali za tieto úlohy:';
+
+  @override
+  String get rainbows_spent_on => 'Dúhy ste minuli na';
+
+  @override
+  String get shop_welcome_text =>
+      'Tu môžete vymeniť dúhy za roztomilé predmety';
+
+  @override
+  String get shop_subtitle_text => 'Vymieňajte dúhy za roztomilé predmety';
+
+  @override
+  String get price => 'Cena';
+
+  @override
+  String get amount => 'Množstvo';
+
+  @override
+  String get total => 'Celkom';
+
+  @override
+  String get order => 'Objednať';
+
+  @override
+  String get order_sent =>
+      'Objednávka bola odoslaná. Kontaktujeme vás e-mailom a informujeme, ako si vyzdvihnúť odmeny.';
+
+  @override
+  String get choose_amount => 'Vyberte množstvo';
+
+  @override
+  String get about_page_title => 'O projekte';
+
+  @override
+  String get about_page_logos => 'Partneri a sponzori projektu';
+
+  @override
+  String get messages_message => 'Správa';
+
+  @override
+  String get delete_account => 'Vymazať účet';
+
+  @override
+  String get delete_account_confirm =>
+      'Účet bude vymazaný do 7 dní. Zmenili ste názor? Kontaktujte info@rainbowchallenge.lt do 3 dní.';
+
+  @override
+  String get wrong_password => 'Nesprávne heslo!';
+
+  @override
+  String get user_logged_in => 'Prihlásený';
+
+  @override
+  String get sign_in_with_google => 'Prihlásiť sa cez Google';
+
+  @override
+  String social_sign_in_failed(String provider) {
+    return 'Prihlásenie cez $provider zlyhalo';
+  }
+
+  @override
+  String get sign_in_with_apple => 'Prihlásiť sa cez Apple';
+
+  @override
+  String get files_attach_file => 'Pripojiť súbor';
+
+  @override
+  String get files_too_big =>
+      'Chyba. Súbor, ktorý sa pokúšate pridať, je príliš veľký';
+
+  @override
+  String get label_active => 'Aktívne';
+}
